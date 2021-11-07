45f352cb9be70656e755f8f1e7457e9ea3ba235f
Júlia Pinheiro <julia_jgp@hotmail.com>
2021-11-07 20:48:11 -0300

    Add files via upload

---
+++ Projeto 16/app_lista_tarefas/conexao.php
@@ -0,0 +1,15 @@
+<?php
+
+    class Conexao {
+
+        private $host = 'localhost';
+        private $dbname = 'php_com_pdo';
+        private $user = 'root';
+        private $pass = '';
+
+        public function conectar() {
+
+        }
+    }
+
+?>
---
+++ Projeto 16/app_lista_tarefas/tarefa.model.php
@@ -0,0 +1,15 @@
+<?php
+    private $id;
+    private $id_status;
+    private $tarefa;
+    private $data_cadastro;
+
+    public function __get($atributo) {
+        return $this->atributo;
+    }
+
+    public function __set($atributo, $valor) {
+        $this->atributo = $valor;
+    }
+
+?>
