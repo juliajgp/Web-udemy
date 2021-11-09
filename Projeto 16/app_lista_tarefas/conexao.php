52d371ed0bac6a799d4a0fb6a63e2ff14d2d0084
Júlia Pinheiro <julia_jgp@hotmail.com>
2021-11-08 22:55:30 -0300

    Add files via upload

--- Projeto 16/app_lista_tarefas/conexao.php
+++ Projeto 16/app_lista_tarefas/conexao.php
@@ -8,7 +8,21 @@
         private $pass = '';
 
         public function conectar() {
+            try {
 
+                $conexao = new PDO(
+                    "mysql:
+                        host=$this->host;
+                        dbname=$this->dbname",
+                    "$this->user",
+                    "$this->pass"
+                );
+
+                return $conexao;
+
+            } catch(PDOException $e) {
+                echo '<p>'.$e->getMessage().'</p>';
+            }
         }
     }
 
--- Projeto 16/app_lista_tarefas/tarefa.service.php
+++ Projeto 16/app_lista_tarefas/tarefa.service.php
@@ -2,6 +2,14 @@
 
     class TarefaService {
 
+        private $conexao;
+        private $tarefa;
+
+        public function __construct(Conexao $conexao, Tarefa $tarefa){
+            $this->conexao = $conexao->conectar();
+            $this->tarefa = $tarefa;
+        }
+
         public function inserir(){
 
         }
