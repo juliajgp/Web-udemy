c9f4fe346a83a6b924ac39eb542a677f76727dab
Júlia Pinheiro <julia_jgp@hotmail.com>
2021-12-20 19:48:34 -0300

    Add files via upload

--- Projeto 16/app_lista_tarefas/tarefa.service.php
+++ Projeto 16/app_lista_tarefas/tarefa.service.php
@@ -11,7 +11,10 @@
         }
 
         public function inserir(){
-
+            $query = 'insert into tb_tarefas(tarefa)values(:tarefa)';
+            $stmt = $this->conexao->prepare($query);
+            $stmt->bindValue(':tarefa', $this->tarefa->__get('tarefa'));
+            $stmt->execute();
         }
         public function recuperar(){
             
