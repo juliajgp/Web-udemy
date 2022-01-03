cbc22aacb1b9a3726511a80c2e129cadb8b87103
Júlia Pinheiro <julia_jgp@hotmail.com>
2022-01-02 23:19:49 -0300

    Add files via upload

--- Projeto 16/app_lista_tarefas/tarefa_controller.php
+++ Projeto 16/app_lista_tarefas/tarefa_controller.php
@@ -4,14 +4,18 @@
     require "../../../app_lista_tarefas/tarefa.service.php";
     require "../../../app_lista_tarefas/conexao.php";
 
-    $tarefa = new Tarefa();
-    $tarefa->__set('tarefa', $_POST['tarefa']);
+    $acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
-    $conexao = new Conexao();
-
-    $tarefaService = new TarefaService($conexao, $tarefa);
-    $tarefaService->inserir();
-
-    header('Location: nova_tarefa.php?inclusao=1');
+    if($acao == 'inserir'){
+        $tarefa = new Tarefa();
+        $tarefa->__set('tarefa', $_POST['tarefa']);
+    
+        $conexao = new Conexao();
+    
+        $tarefaService = new TarefaService($conexao, $tarefa);
+        $tarefaService->inserir();
+    
+        header('Location: nova_tarefa.php?inclusao=1');
+    }
 
 ?>
