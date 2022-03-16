f9ce46a3ffd087d9534206d5080a18502d9096a7
Júlia Pinheiro <julia_jgp@hotmail.com>
2022-03-16 03:26:41 -0300

    Add files via upload

--- Projeto 11/app.js
+++ Projeto 11/app.js
@@ -40,6 +40,24 @@ class Bd {
         localStorage.setItem(id, JSON.stringify(d));
         localStorage.setItem('id', id);
     }
+
+    recuperarTodosRegistros() {
+
+        let despesas = Array();
+
+        let id = localStorage.getItem('id');
+
+        for(let i = 1; i <= id; i++) {
+            let despesa = JSON.parse(localStorage.getItem(i));
+
+            if(despesa === null) {
+                continue;
+            }
+            despesas.push(despesa);
+        }
+
+        return despesas;
+    }
 }
 
 let bd = new Bd();
@@ -84,7 +102,11 @@ function cadastrarDespesa(){
 
         $('#modalRegistraDespesa').modal('show');
     }
-
-    
 }
 
+
+function carregaListaDespesas() {
+    let despesas = Array();
+    despesas = bd.recuperarTodosRegistros();
+    console.log(despesas);
+}
