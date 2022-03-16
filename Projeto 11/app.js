bce273397fa955955f7daec9937596c276337917
Júlia Pinheiro <julia_jgp@hotmail.com>
2022-03-16 04:04:29 -0300

    Add files via upload

--- Projeto 11/app.js
+++ Projeto 11/app.js
@@ -81,7 +81,7 @@ function cadastrarDespesa(){
     );
 
     if(despesa.validarDados()) {
-        //bd.gravar(despesa);
+        bd.gravar(despesa);
 
         document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso';
         document.getElementById('modal_titulo_div').className = 'modal-header text-success';
@@ -116,6 +116,25 @@ function carregaListaDespesas() {
 
         //linha.insertCell(0).innerHTML = d.dia + '/' + d.mes + '/' + d.ano;
         linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`;
+
+        switch(d.tipo) {
+            case '1':
+                d.tipo = 'Alimentação';
+                break;
+            case '2':
+                d.tipo = 'Educação';
+                break;
+            case '3':
+                d.tipo = 'Lazer';
+                break;
+            case '4':
+                d.tipo = 'Saúde';
+                break;
+            case '5':
+                d.tipo = 'Transporte';
+                break;
+        }
+
         linha.insertCell(1).innerHTML = d.tipo;
         linha.insertCell(2).innerHTML = d.descricao;
         linha.insertCell(3).innerHTML = d.valor;
