925fc8ce30dac7a8051c9f66edaffa8fcbcc0c5e
Júlia Pinheiro <julia_jgp@hotmail.com>
2022-03-19 03:54:35 -0300

    Add files via upload

--- Projeto 11/app.js
+++ Projeto 11/app.js
@@ -53,6 +53,8 @@ class Bd {
             if(despesa === null) {
                 continue;
             }
+
+            despesa.id = i;
             despesas.push(despesa);
         }
 
@@ -94,6 +96,10 @@ class Bd {
         }
 
         return despesasFiltradas;
+    }
+
+    remover(id) {
+        localStorage.removeItem(id);
     }
 }
 
@@ -186,6 +192,22 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
         linha.insertCell(1).innerHTML = d.tipo;
         linha.insertCell(2).innerHTML = d.descricao;
         linha.insertCell(3).innerHTML = d.valor;
+
+        //criar botão exclusão
+        let btn = document.createElement("button");
+        btn.className = 'btn btn-danger';
+        btn.innerHTML = '<i class="fas fa-times"></i>';
+        btn.id = `id_despesa_${d.id}`;
+        
+        btn.onclick = function () {
+            //remover a despesa
+            let id = this.id.replace('id_despesa_', '');
+            bd.remover(id);
+
+            window.location.reload();
+        }
+        linha.insertCell(4).append(btn);
+
     })
 }
 
