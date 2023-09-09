d8d8a96b9666c04777f0a3a9d29821f3b0f99618
Júlia Pinheiro <julia_jgp@hotmail.com>
2023-09-08 22:56:16 -0300

    Add files via upload

---
+++ Projeto 12/valida_login.php
@@ -0,0 +1,16 @@
+<?php
+
+    //usuarios do sistema
+    $usuarios_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+    );
+
+    echo '<pre>';
+    print_r($usuarios_app);
+    echo '</pre>';
+
+    //$_POST['email'];
+    //$_POST['senha'];  
+
+?>
