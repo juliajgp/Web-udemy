8be964091449a0d93804297f61bee363a53c3731
Júlia Pinheiro <julia_jgp@hotmail.com>
2023-09-08 23:44:01 -0300

    Add files via upload

--- Projeto 12/index.php
+++ Projeto 12/index.php
@@ -31,16 +31,28 @@
             <div class="card-header">
               Login
             </div>
+
             <div class="card-body">
-              <form>
+
+              <form action="valida_login.php" method="post">
                 <div class="form-group">
-                  <input type="email" class="form-control" placeholder="E-mail">
+                  <input name="email" type="email" class="form-control" placeholder="E-mail">
                 </div>
                 <div class="form-group">
-                  <input type="password" class="form-control" placeholder="Senha">
+                  <input name="senha" type="password" class="form-control" placeholder="Senha">
                 </div>
+
+                <? if(isset($_GET['login']) && $_GET['login'] == 'erro'){ ?>
+                
+                  <div class="text-danger">
+                    Usuário ou senha inválidos
+                  </div> 
+                    
+                <? } ?>
+
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
+
             </div>
           </div>
         </div>
--- Projeto 12/valida_login.php
+++ Projeto 12/valida_login.php
@@ -1,14 +1,32 @@
 <?php
 
-    //usuarios do sistema
+    //variavel que verifica se a aut. foi realizada
+    $usuario_autenticado = false;
+
+    //usuarios do sistema (hardcode)
     $usuarios_app = array(
         array('email' => 'adm@teste.com.br', 'senha' => '123456'),
         array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
     );
 
+    /*
     echo '<pre>';
     print_r($usuarios_app);
     echo '</pre>';
+    */
+
+    foreach($usuarios_app as $user){
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+        }
+    }
+
+    if($usuario_autenticado){
+        echo 'Usuário autenticado';
+    }
+    if(!$usuario_autenticado){
+        header('Location: index.php?login=erro');
+    }
 
     //$_POST['email'];
     //$_POST['senha'];  
