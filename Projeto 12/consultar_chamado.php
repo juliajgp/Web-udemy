7fb43ad5695160a5a946933d838ca161e17ee320
Júlia Pinheiro <julia_jgp@hotmail.com>
2023-09-22 21:17:23 -0300

    Add files via upload

--- Projeto 12/consultar_chamado.php
+++ Projeto 12/consultar_chamado.php
@@ -1,10 +1,16 @@
 <?php
-  session_start();
+  require_once("validador_acesso.php");
 
-  if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM'){
-    header('Location: index.php?login=erro2');
+  $chamados = array();
+
+  $arquivo = fopen('arquivo.hd', 'r');
+
+  while(!feof($arquivo)){
+    $registro = fgets($arquivo);
+    $chamados[] = $registro;
   }
 
+  fclose($arquivo);
 ?>
 
 <html>
@@ -30,6 +36,12 @@
         <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
         App Help Desk
       </a>
+
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="logoff.php">SAIR</a>
+        </li>
+      </ul>
     </nav>
 
     <div class="container">    
@@ -43,27 +55,35 @@
             
             <div class="card-body">
               
+              <?php foreach($chamados as $chamado) { ?>
+
+                <?php 
+                  $chamado_dados = explode('#', $chamado);
+
+                  if($_SESSION['perfil_id'] == 2){
+                    if($_SESSION['id'] != $chamado_dados[0]){
+                      continue;
+                    }
+                  }
+
+                  if(count($chamado_dados) < 4){
+                    continue;
+                  }
+                ?>
+
               <div class="card mb-3 bg-light">
                 <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
+                  <h5 class="card-title"><?=$chamado_dados[1]?></h5>
+                  <h6 class="card-subtitle mb-2 text-muted"><?=$chamado_dados[2]?></h6>
+                  <p class="card-text"><?=$chamado_dados[3]?></p>
                 </div>
               </div>
 
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
+              <?php } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
-                  <button class="btn btn-lg btn-warning btn-block" type="submit">Voltar</button>
+                  <a class="btn btn-lg btn-warning btn-block" href="home.php">Voltar</a>
                 </div>
               </div>
             </div>
--- Projeto 12/valida_login.php
+++ Projeto 12/valida_login.php
@@ -4,11 +4,17 @@
 
     //variavel que verifica se a aut. foi realizada
     $usuario_autenticado = false;
+    $usuario_id = null;
+    $usuario_perfil_id = null;
+
+    $perfis = array(1 => 'Administrativo', 2 => 'Usuario');
 
     //usuarios do sistema (hardcode)
     $usuarios_app = array(
-        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
-        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+        array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+        array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+        array('id' => 3, 'email' => 'jose@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
+        array('id' => 4, 'email' => 'maria@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
     );
 
     /*
@@ -20,12 +26,17 @@
     foreach($usuarios_app as $user){
         if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
             $usuario_autenticado = true;
+            $usuario_id = $user['id'];
+            $usuario_perfil_id = $user['perfil_id'];
         }
     }
 
     if($usuario_autenticado){
         echo 'Usuário autenticado';
         $_SESSION['autenticado'] = 'SIM';
+        $_SESSION['id'] = $usuario_id;
+        $_SESSION['perfil_id'] = $usuario_perfil_id;
+        header('Location: home.php');
     }
     if(!$usuario_autenticado){
         $_SESSION['autenticado'] = 'NÃO';
